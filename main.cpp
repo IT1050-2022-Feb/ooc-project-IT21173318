d0b9cd46582eac8d9092dd7c718e956c90876074
IT21173318 <99825826+IT21173318@users.noreply.github.com>
2022-05-20 20:44:10 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 int main(){
-	
+	//Vacancy
 	vacancy v1(0001,"Software Engineer","Technical","Primary Duties and responsibility and education qualification","OrangeHRM Inc.");
 	v1.displayvacancy();
 	v1.searchvacancy();
@@ -20,6 +20,21 @@ int main(){
 	v2.deletevacancy();
 
 	cout << ".........................................................................................................................."<<endl;
+	
+	
+	//Report
+	
+		
+	Report R1(6556,"Interview report","1GB");
+	R1.displayReportDetails();
+	R1.deleteReport();
+	
+	cout << "..................................................................................................................."<<endl;
+
+	Report R2;
+	R2.addReport();
+	R2.displayReportDetails();
+	R2.deleteReport();
 	
 	return 0;
 	
